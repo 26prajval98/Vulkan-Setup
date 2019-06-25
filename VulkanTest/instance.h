7a2ba6595e69f7f18b65ffdaef8b37fece350ee9
26prajval98 <26prajval98@gmail.com>
2019-06-25 19:51:28 +0530

    some shit

--- VulkanTest/instance.h
+++ VulkanTest/instance.h
@@ -144,8 +144,8 @@ public:
 
 private:
 	VkInstance m_instance;
-	//std::vector<const char *>extensions;
-	//std::vector<const char *>layers;
+	std::vector<const char *>extensions;
+	std::vector<const char *>layers;
 	void cleanUp() {
 		vkDestroyInstance(m_instance, nullptr);
 	}
@@ -155,29 +155,29 @@ Instance::Instance()
 {
 	VkResult res;
 	
-	//// Instance creation
-	//layers.push_back("VK_LAYER_LUNARG_standard_validation");
-	//uint32_t glfwExtensionCount = 0;
-	//const char** glfwExtensions;
+	// Instance creation
+	layers.push_back("VK_LAYER_LUNARG_standard_validation");
+	uint32_t glfwExtensionCount = 0;
+	const char** glfwExtensions;
 
-	//glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
-	////const char* extensions_array[] = {
-	////	"VK_EXT_debug_report",
-	////	"VK_KHR_surface",
-	////};
+	glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
+	//const char* extensions_array[] = {
+	//	"VK_EXT_debug_report",
+	//	"VK_KHR_surface",
+	//};
 
-	//extensions = std::vector<const char *>(glfwExtensions, glfwExtensions + glfwExtensionCount);
-	//
-	//extensions.push_back("VK_EXT_debug_report");
+	extensions = std::vector<const char *>(glfwExtensions, glfwExtensions + glfwExtensionCount);
+	
+	extensions.push_back("VK_EXT_debug_report");
 
-	//VkApplicationInfo appInfo = initialiser::createAppInfo();
-	//VkInstanceCreateInfo createInfo = initialiser::createInstanceInfo(appInfo, extensions, layers);
-	//res = vkCreateInstance(&createInfo, nullptr, &m_instance);
+	VkApplicationInfo appInfo = initialiser::createAppInfo();
+	VkInstanceCreateInfo createInfo = initialiser::createInstanceInfo(appInfo, extensions, layers);
+	res = vkCreateInstance(&createInfo, nullptr, &m_instance);
 
-	//assert(VK_SUCCESS == res && "INSTANCE ERROR");
+	assert(VK_SUCCESS == res && "INSTANCE ERROR");
 
-	createInstance();
-	m_instance = instancee;
+	//createInstance();
+	//m_instance = instancee;
 }
 
 Instance::~Instance()
